978279e84497a75313a9f1c8e83c444a7e59db0b
asj397 <asj397@nyu.edu>
2021-02-25 15:37:27 -0500

    Expanded Data Storage

--- Assets/Scripts/Week 4/ChessSolver.cs
+++ Assets/Scripts/Week 4/ChessSolver.cs
@@ -7,7 +7,18 @@ using System;
 public class ChessSolver : MonoBehaviour
 {
     public const int BOARDLENGTH = 8;
+    public readonly Vector2Int[] PAWNTHREAT = { new Vector2Int(1, 1), new Vector2Int(1, -1) };
+    //public readonly Vector2Int[] PAWNTHREAT = { new Vector2Int(1, 1), new Vector2Int(1, -1) };
+
+    //public readonly Vector2Int[] PAWNTHREAT = { new Vector2Int(1, 1), new Vector2Int(1, -1) };
+
+    //public readonly Vector2Int[] PAWNTHREAT = { new Vector2Int(1, 1), new Vector2Int(1, -1) };
+
+    //public readonly Vector2Int[] PAWNTHREAT = { new Vector2Int(1, 1), new Vector2Int(1, -1) };
+
+
     public ChessPiece[,] board = new ChessPiece[BOARDLENGTH, BOARDLENGTH];
+    public ChessThreat[,] threatBoard = new ChessThreat[BOARDLENGTH, BOARDLENGTH];
 
     // Start is called before the first frame update
     void Start()
@@ -65,51 +76,141 @@ public class ChessSolver : MonoBehaviour
         {
             for (int j = 0; j < BOARDLENGTH; j++)
             {
-                board[i,j] = CharToChessPiece(boardInfoChar[i + j]);
+                board[i,j] = CharToChessPiece(boardInfoChar[i + j], new Vector2Int(i, j));
                 Debug.Log(board[i, j]);
             }
         }
     }
 
-    private ChessPiece CharToChessPiece(char chessChar)
+    private ChessPiece CharToChessPiece(char chessChar, Vector2Int location)
     {
         switch (chessChar)
         {
             case ('p'):
-                return new ChessPiece(ChessPieceType.pawn, ChessPieceTeam.white);
+                return new ChessPiece(ChessPieceType.pawn, ChessPieceTeam.white, location);
             case ('P'):
-                return new ChessPiece(ChessPieceType.pawn, ChessPieceTeam.black);
+                return new ChessPiece(ChessPieceType.pawn, ChessPieceTeam.black, location);
             case ('b'):
-                return new ChessPiece(ChessPieceType.bishop, ChessPieceTeam.white);
+                return new ChessPiece(ChessPieceType.bishop, ChessPieceTeam.white, location);
             case ('B'):
-                return new ChessPiece(ChessPieceType.bishop, ChessPieceTeam.black);
+                return new ChessPiece(ChessPieceType.bishop, ChessPieceTeam.black, location);
             case ('n'):
-                return new ChessPiece(ChessPieceType.knight, ChessPieceTeam.white);
+                return new ChessPiece(ChessPieceType.knight, ChessPieceTeam.white, location);
             case ('N'):
-                return new ChessPiece(ChessPieceType.knight, ChessPieceTeam.black);
+                return new ChessPiece(ChessPieceType.knight, ChessPieceTeam.black, location);
             case ('r'):
-                return new ChessPiece(ChessPieceType.rook, ChessPieceTeam.white);
+                return new ChessPiece(ChessPieceType.rook, ChessPieceTeam.white, location);
             case ('R'):
-                return new ChessPiece(ChessPieceType.rook, ChessPieceTeam.black);
+                return new ChessPiece(ChessPieceType.rook, ChessPieceTeam.black, location);
             case ('q'):
-                return new ChessPiece(ChessPieceType.queen, ChessPieceTeam.white);
+                return new ChessPiece(ChessPieceType.queen, ChessPieceTeam.white, location);
             case ('Q'):
-                return new ChessPiece(ChessPieceType.queen, ChessPieceTeam.black);
+                return new ChessPiece(ChessPieceType.queen, ChessPieceTeam.black, location);
             case ('k'):
-                return new ChessPiece(ChessPieceType.king, ChessPieceTeam.white);
+                return new ChessPiece(ChessPieceType.king, ChessPieceTeam.white, location);
             case ('K'):
-                return new ChessPiece(ChessPieceType.king, ChessPieceTeam.black);
+                return new ChessPiece(ChessPieceType.king, ChessPieceTeam.black, location);
             default:
-                return new ChessPiece(ChessPieceType.none, ChessPieceTeam.none);
+                return new ChessPiece(ChessPieceType.none, ChessPieceTeam.none, new Vector2Int(-1, -1));
         }
     }
 
     public Vector2Int[] GetAllNextMoves(int length, int width)
     {
         ChessPiece chessPiece = board[length, width];
-        List<Vector2Int> nextMoveArray = new List<Vector2Int>();
-        if (chessPiece.chessPieceType == ChessPieceType.pawn && chespi)
+        List<Vector2Int> possibleLocations = new List<Vector2Int>();
+        switch (chessPiece.chessPieceType)
+        {
+            case (ChessPieceType.pawn):
+                if (chessPiece.team == ChessPieceTeam.white)
+                {
+                    if (length + 1 < 8 && board[length + 1, width].chessPieceType == ChessPieceType.none)
+                    {
+                        possibleLocations.Add(new Vector2Int(length + 1, width));
+                    }
+                }
+                else
+                {
+                    if (length - 1 > 0 && board[length - 1, width].chessPieceType == ChessPieceType.none)
+                    {
+                        possibleLocations.Add(new Vector2Int(length - 1, width));
+                    }
+                }
+                break;
+        }
+            
     }
+
+    public Vector2Int[] GetAllNextMoves(int length, int width)
+    {
+        ChessPiece chessPiece = board[length, width];
+        List<Vector2Int> possibleLocations = new List<Vector2Int>();
+        switch (chessPiece.chessPieceType)
+        {
+            case (ChessPieceType.pawn):
+                if (chessPiece.team == ChessPieceTeam.white)
+                {
+                    if (length + 1 < 8 && board[length + 1, width].chessPieceType == ChessPieceType.none)
+                    {
+                        possibleLocations.Add(new Vector2Int(length + 1, width));
+                    }
+                }
+                else
+                {
+                    if (length - 1 > 0 && board[length - 1, width].chessPieceType == ChessPieceType.none)
+                    {
+                        possibleLocations.Add(new Vector2Int(length - 1, width));
+                    }
+                }
+                break;
+        }
+
+    }
+
+    public List<ChessThreat> CalculateThreatSpaces(ChessPiece chessPiece)
+    {
+        switch (chessPiece.chessPieceType)
+        {
+            case (ChessPieceType.pawn):
+                if (chessPiece.team == ChessPieceTeam.black)
+                {
+                    List<Vector2Int> threatLocations = new List<Vector2Int>();
+                    Vector2Int temp;
+                    temp = chessPiece.location + new Vector2Int(1, 1);
+                    if (IsLocationValid(temp)) { threatLocations.Add(temp); }
+
+
+                }
+                break;
+        }
+    }
+
+    public bool Check()
+    {
+
+    }
+
+    public bool CheckMate()
+    {
+
+    }
+
+    public bool IsLocationValid(Vector2Int location)
+    {
+        if (location.x < 8 && location.x >= 0 && location.y < 8 && location.y > 0 && board[location.x, location.y].chessPieceType == ChessPieceType.none)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+
+
 
     public enum ChessPieceType
     {
@@ -134,16 +235,57 @@ public struct ChessPiece
 {
     public ChessSolver.ChessPieceType chessPieceType;
     public ChessSolver.ChessPieceTeam team;
+    public Vector2Int location;
 
-    public ChessPiece(ChessSolver.ChessPieceType chessPieceType, ChessSolver.ChessPieceTeam team)
+    public ChessPiece(ChessSolver.ChessPieceType chessPieceType, ChessSolver.ChessPieceTeam team, Vector2Int location)
     {
         this.chessPieceType = chessPieceType;
         this.team = team;
+        this.location = location;
     }
 
     public override string ToString()
     {
         if (chessPieceType == ChessSolver.ChessPieceType.none || team == ChessSolver.ChessPieceTeam.none) { return "Empty space"; }
-        return $"{team} {chessPieceType}";
+        return $"{team} {chessPieceType} ({location.x}, {location.y})";
+    }
+}
+
+public struct ChessThreat
+{
+    public List<ChessPiece> chessPieces;
+    public Vector2Int location;
+
+    public ChessThreat(List<ChessPiece> chessPieces, Vector2Int location)
+    {
+        this.chessPieces = chessPieces;
+        this.location = location;
+    }
+
+    public bool IsLocationThreatened(ChessSolver.ChessPieceTeam chessPieceTeam)
+    {
+        if (chessPieceTeam == ChessSolver.ChessPieceTeam.white)
+        {
+            foreach (ChessPiece chessPiece in chessPieces)
+            {
+                if (chessPiece.team == ChessSolver.ChessPieceTeam.black)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        else if (chessPieceTeam == ChessSolver.ChessPieceTeam.black)
+        {
+            foreach (ChessPiece chessPiece in chessPieces)
+            {
+                if (chessPiece.team == ChessSolver.ChessPieceTeam.white)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
     }
 }
